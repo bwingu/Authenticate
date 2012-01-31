413a186ef946b40ce13857fe2219eef337dec438
himurafred <himurafred@gmail.com>
2012-01-31 23:32:15 +0100

    début roles

--- app/models/Check.java
+++ app/models/Check.java
@@ -8,4 +8,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface Check {
+	//Les roles
+	String [] roles();
 }
